69f8c7937c9a439f508bc4f66e2d1da4607de0b3
ShadyXGuy <156676283+ShadyXGuy@users.noreply.github.com>
2024-09-23 11:25:15 +0200

    lister for for alfabet/morse kode

--- morse.py
+++ morse.py
@@ -5,12 +5,14 @@ alfabet=[' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'
 
 # Dictionary til oversættelse fra bogstaver til morsekode
 morseCode = {}
-
+for i in range(len(alfabet)):
+    morseCode[alfabet[i]]=morse[i]
 
 
 # Dictionary til oversættelse fra morsekode til bogstaver. Tomt oversættes til mellemrum.
 morseCodeReverse = {}
-
+for i in range(len(morse)):
+    morseCode[morse[i]]=alfabet[i]
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
