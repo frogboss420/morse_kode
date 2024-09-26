56eb3e5c8fe4f2b79bd7af219f38f15e2e566d0d
madsf <mads.friis.nielsen@gmail.com>
2024-09-26 14:18:37 +0200

    RIOT LEAUGEO FL EGNEDS CLIENT PATCH BUG FIXSE!!!.cn
    **TRANSLATE FIXED
    **ALPHABET AND MORSE LIST FIXED
    **herobrine removed

--- morse.py
+++ morse.py
@@ -1,26 +1,23 @@
 #lister til alfabet og morsekode for at definere key og value
-morse=['//','.-','-..','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','-.','--','---','.--.','--.-','.-.','...','-','.--','...-','.--','-..-','-.--','--..','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----','.-.-.-','--..--','---...','..--..','.----.','-....-','-..-.','-.--.','-.--.-','.-..-.','.--.-.']
+morse=['//','.-','-..','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','-.','--','---','.--.','--.-','.-.','...','-','.--','...-','.--','-..-','-.--','--..','.-.-','---.','.--.-','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----','.-.-.-','--..--','---...','..--..','.----.','-....-','-..-.','-.--.','-.--.-','.-..-.','.--.-.']
 
 alfabet=[' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Æ','Ø','Å','1','2','3','4','5','6','7','8','9','0','.',',',':','?',"'",'-','/','(',')','"','@']
+print(len(morse))
+print(len(alfabet))
 
 # Dictionary til oversættelse fra bogstaver til morsekode
-morseCode = {}
-for i in range(len(alfabet)):
-    morseCode[alfabet[i]]=morse[i]
-
-
 # Dictionary til oversættelse fra morsekode til bogstaver. Tomt oversættes til mellemrum.
+morseCode = {}
 morseCodeReverse = {}
-for i in range(len(morse)):
-    morseCode[morse[i]]=alfabet[i]
+for i in range(len(alfabet)-1):
+    morseCode[alfabet[i]]=morse[i]
+    morseCodeReverse[morse[i]]=alfabet[i]
+
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
-    if letter in alfabet:
+    if letter in code:
         return code[letter]
-    elif letter in morse:
-        return letter[code]
-
 
 
 # Denne funktion oversætter en vilkårlig tekststreng til morsekode
