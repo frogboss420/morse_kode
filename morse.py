bb50d6ee50f36998329b13098226e5cb97e28481
madsf <mads.friis.nielsen@gmail.com>
2024-09-26 15:00:16 +0200

    RIOT LEAUGEO FL EGNEDS CLIENT PATCH BUG FIXSE!!!.cn
    **FULL RELEASE OF ENCODEMESSAGE!!
    
    **EARLY ACCESS GET:
    FIXED -1 LENGTH OF MESSAGE BUG

--- morse.py
+++ morse.py
@@ -1,5 +1,5 @@
 #lister til alfabet og morsekode for at definere key og value
-morse=['//','.-','-..','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','-.','--','---','.--.','--.-','.-.','...','-','.--','...-','.--','-..-','-.--','--..','.-.-','---.','.--.-','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----','.-.-.-','--..--','---...','..--..','.----.','-....-','-..-.','-.--.','-.--.-','.-..-.','.--.-.']
+morse=['','.-','-..','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','-.','--','---','.--.','--.-','.-.','...','-','.--','...-','.--','-..-','-.--','--..','.-.-','---.','.--.-','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----','.-.-.-','--..--','---...','..--..','.----.','-....-','-..-.','-.--.','-.--.-','.-..-.','.--.-.']
 
 alfabet=[' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Æ','Ø','Å','1','2','3','4','5','6','7','8','9','0','.',',',':','?',"'",'-','/','(',')','"','@']
 print(len(morse))
@@ -9,14 +9,14 @@ print(len(alfabet))
 # Dictionary til oversættelse fra morsekode til bogstaver. Tomt oversættes til mellemrum.
 morseCode = {}
 morseCodeReverse = {}
-for i in range(len(alfabet)-1):
+for i in range(len(alfabet)):
     morseCode[alfabet[i]]=morse[i]
     morseCodeReverse[morse[i]]=alfabet[i]
 
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
-    if letter in code:
+    if str(letter) in code:
         return code[letter]
     else:
         return '?'
@@ -26,12 +26,11 @@ def translate(letter, code):
 # '/' markerer nyt bogstav
 # '//' markerer nyt ord
 def encodeMessage(message, code):
-    for i in range (len(message)-1):
-        translate(message[i],code)
-
-#temporary debug
-message = 'SUP BIG CHUNGUS'
-print(encodeMessage(message,morseCode))
+    message = str(message)
+    out=''
+    for i in range (len(message)):
+        out+=translate(message[i],code)+'/'
+    return out
 
 # Denne funktion oversætter en korrekt formatteret morsebesked til bogstaver
 # '/' markerer nyt bogstav
